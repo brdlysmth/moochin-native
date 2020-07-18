f1671fdb18662604e5ec1142f6feea4d261e1e44
brdlysmth <brsmith110@gmail.com>
2020-07-18 18:56:06 -0400

    can add things to firebase now

--- components/BookFormScreenInfo.tsx
+++ components/BookFormScreenInfo.tsx
@@ -1,28 +1,54 @@
 import * as React from 'react'; 
 import { Formik } from 'formik';
 import { Text } from './Themed';
-import { TextInput, View } from 'react-native';
-import { Button } from 'react-native-material-ui';
+import { Button, TextInput, View } from 'react-native';
+// import { Button } from 'react-native-material-ui';
+import Modal from 'react-native-modal';
+import BookFormScreen from '../screens/BookFormScreen';
 
 export default function BookFormScreenInfo({ path }: { path: string }) {
+    
+    const [isModalVisible, setIsModalVisible] = React.useState(false);
+    const toggleModal = () => {
+        setIsModalVisible(!isModalVisible);
+    };
+
     return (
-        <Formik
-        initialValues={{ email: '' }}
-        onSubmit={values => console.log(values)}
-        >
-        {({ handleChange, handleBlur, handleSubmit, values }) => (
-            <View>
-            <TextInput
-                onChangeText={handleChange('email')}
-                onBlur={handleBlur('email')}
-                value={values.email}
-            />
-            <Button 
-                onPress={handleSubmit} 
-                text="Submit"
-                />
-            </View>
-        )}
-        </Formik>
-    );
-  }
+        <View>
+            <Button title="Click here to login" onPress={toggleModal} />
+
+            <Modal
+                isVisible={isModalVisible}>
+                <View>
+                    <BookFormScreen />
+                    <View>
+                        <Button 
+                            title="Hide modal" 
+                            onPress={toggleModal} 
+                        />
+                        
+                    </View>
+                </View>
+            </Modal>
+
+        </View>
+    )
+};
+
+// const styles = StyleSheet.create({
+// container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     backgroundColor: 'white',
+// },
+// title: {
+//     fontSize: 20,
+//     fontWeight: 'bold',
+// },
+// separator: {
+//     marginVertical: 30,
+//     height: 1,
+//     width: '80%',
+// },
+// });
--- screens/BookFormScreen.tsx
+++ screens/BookFormScreen.tsx
@@ -8,6 +8,7 @@ import Modal from 'react-native-modal';
 import BookFormScreenInfo from '../components/BookFormScreenInfo';
 import { Text, View } from '../components/Themed';
 import Navigation from '../navigation/index';
+import { db } from '../App';
 
 export default function BookFormScreen() {
 
@@ -17,6 +18,14 @@ export default function BookFormScreen() {
     // yarn add react-native-photo-upload
 
     const [isModalVisible, setModalVisible] = React.useState(false);
+    const [value, onChangeText] = React.useState('Useless Placeholder');
+
+    const addBook = async ( value: any ) => {
+        await db.ref('/books').push({
+            new: 'true',
+            name: value,
+        })
+    }
   
     const toggleModal = () => {
         setModalVisible(!isModalVisible);
@@ -28,12 +37,34 @@ export default function BookFormScreen() {
                 Add Book 
             </Text>
 
-            <Button title="Show modal" onPress={toggleModal} />
+            <Button title="Add Book" onPress={toggleModal} />
 
-            <Modal isVisible={ isModalVisible }>
-                <View style={{ flex: 1 }}>
-                    <Text>I am the modal content!</Text>
-                    <Button title="Hide modal" onPress={toggleModal} />
+            <Modal 
+                isVisible={ isModalVisible }
+                onBackdropPress={() => setModalVisible(false)}
+                onSwipeComplete={() => setModalVisible(false)}
+                swipeDirection="left"
+                >
+                <View style={styles.container}>
+                    <Text>
+                        Love to see some books getting added.
+                    </Text>
+                    
+                    <TextInput
+                        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+                        onChangeText={text => onChangeText(text)}
+                        value={value}
+                        placeholder='Enter email... '
+                        onSubmitEditing={() => addBook(value)}
+                        >
+
+                    </TextInput>
+
+                    <Button 
+                        title="Hide modal" 
+                        onPress={toggleModal} 
+                        />
+
                 </View>
 
             </Modal>
@@ -42,21 +73,22 @@ export default function BookFormScreen() {
     );
   }
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor: 'white',
-    },
-    title: {
-      fontSize: 20,
-      fontWeight: 'bold',
-    },
-    separator: {
-      marginVertical: 30,
-      height: 1,
-      width: '80%',
-    },
-  });
+
+const styles = StyleSheet.create({
+container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+},
+title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+},
+separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+},
+});
   
