cc6c3d4c8e49e214936aa8acac7bc491706ed51f
brdlysmth <brsmith110@gmail.com>
2020-07-22 21:59:21 -0400

    removing leftover text

--- services/auth.tsx
+++ services/auth.tsx
@@ -7,7 +7,6 @@ export const firebaseAuth: FirebaseAuth = (firebase as any).auth();
 export const logout = () =>
   firebaseAuth.signOut();
 
-<<<<<<< HEAD
 export const loginUserInFirebase = (email: string, pw: string) =>
   firebaseAuth.signInWithEmailAndPassword(email, pw);
 
