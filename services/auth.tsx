75ed51b213453a5bd91a984c47054042482c197d
brdlysmth <brsmith110@gmail.com>
2020-07-22 20:58:43 -0400

    adding firebase auth

---
+++ services/auth.tsx
@@ -0,0 +1,29 @@
+import firebase from '@firebase/app';
+import '@firebase/auth';
+import { FirebaseAuth, Error } from '@firebase/auth-types';
+
+export const firebaseAuth: FirebaseAuth = (firebase as any).auth();
+
+export const logout = () =>
+  firebaseAuth.signOut();
+
+export const login = (email: string, pw: string) =>
+  firebaseAuth.signInWithEmailAndPassword(email, pw);
+
+export const loginErrorToText = (err: Error) => {
+  switch (err.code) {
+    case 'auth/invalid-email':
+      return 'Invalid email address';
+    case 'auth/user-disabled':
+      return 'Account has been disabled';
+    case 'auth/user-not-found':
+      return 'Account does not exist';
+    case 'auth/wrong-password':
+      return 'Incorrect password';
+    default:
+      return 'An unknown error occured';
+  }
+};
+
+export const resetPassword = (email: string) =>
+  firebaseAuth.sendPasswordResetEmail(email);
